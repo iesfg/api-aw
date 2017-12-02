d6a06a29ab31cc66473f2d918654d8cb3d2796e0
iesfg <informatica.iesfuentegrande@gmail.com>
2017-12-02 09:39:51 +0100

    api-aw iesfg

--- controllers/api/users.controller.js
+++ controllers/api/users.controller.js
@@ -6,7 +6,7 @@ var userService = require('services/user.service');
 // routes
 router.post('/authenticate', authenticateUser);
 router.post('/register', registerUser);
-router.get('/current', getCurrentUser);
+router.get('/userprofile', getCurrentUser);
 router.put('/:_id', updateUser);
 router.delete('/:_id', deleteUser);
 
@@ -17,9 +17,11 @@ function authenticateUser(req, res) {
         .then(function (token) {
             if (token) {
                 // authentication successful
+                res.header('Access-Control-Allow-Origin', '*');
                 res.send({ token: token });
             } else {
                 // authentication failed
+                res.header('Access-Control-Allow-Origin', '*');
                 res.status(401).send('Username or password is incorrect');
             }
         })
@@ -31,9 +33,11 @@ function authenticateUser(req, res) {
 function registerUser(req, res) {
     userService.create(req.body)
         .then(function () {
+            res.header('Access-Control-Allow-Origin', '*');
             res.sendStatus(200);
         })
         .catch(function (err) {
+            res.header('Access-Control-Allow-Origin', '*');
             res.status(400).send(err);
         });
 }
@@ -42,28 +46,35 @@ function getCurrentUser(req, res) {
     userService.getById(req.user.sub)
         .then(function (user) {
             if (user) {
+                res.header('Access-Control-Allow-Origin', '*');
                 res.send(user);
             } else {
+                res.header('Access-Control-Allow-Origin', '*');
                 res.sendStatus(404);
             }
         })
         .catch(function (err) {
+            res.header('Access-Control-Allow-Origin', '*');
             res.status(400).send(err);
         });
 }
+
 
 function updateUser(req, res) {
     var userId = req.user.sub;
     if (req.params._id !== userId) {
         // can only update own account
+        res.header('Access-Control-Allow-Origin', '*');
         return res.status(401).send('You can only update your own account');
     }
 
     userService.update(userId, req.body)
         .then(function () {
+            res.header('Access-Control-Allow-Origin', '*');
             res.sendStatus(200);
         })
         .catch(function (err) {
+            res.header('Access-Control-Allow-Origin', '*');
             res.status(400).send(err);
         });
 }
@@ -71,15 +82,18 @@ function updateUser(req, res) {
 function deleteUser(req, res) {
     var userId = req.user.sub;
     if (req.params._id !== userId) {
+        res.header('Access-Control-Allow-Origin', '*');
         // can only delete own account
         return res.status(401).send('You can only delete your own account');
     }
 
     userService.delete(userId)
         .then(function () {
+            res.header('Access-Control-Allow-Origin', '*');
             res.sendStatus(200);
         })
         .catch(function (err) {
+            res.header('Access-Control-Allow-Origin', '*');
             res.status(400).send(err);
         });
 }
